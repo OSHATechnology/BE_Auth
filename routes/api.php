b4b1f6582e763612e98b353e198d7a53263c2c32
rizky <rizkyaditya2106@gmail.com>
2022-09-20 12:55:59 +0700

    second commit

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminAccessController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +19,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('admin_access/register', [AuthController::class, 'register']);
+Route::post('admin_access/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('admin_access', [AdminAccessController::class, 'index']);
+    Route::post('admin_access/store', [AdminAccessController::class, 'store']);
+    Route::get('admin_access/show/{id}', [AdminAccessController::class, 'show']);
+    Route::put('admin_access/update/{id}', [AdminAccessController::class, 'update']);
+    Route::delete('admin_access/destroy/{id}', [AdminAccessController::class, 'destroy']);
+    Route::post('admin_access/logout', [AuthController::class, 'logout']);
 });
